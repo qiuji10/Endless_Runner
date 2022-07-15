ab51f9acf9217b010e1d1376881d3e28b16f45a6
qiuji <74701224+qiuji10@users.noreply.github.com>
2022-07-15 17:52:32 +0800

    Remove all unecessary thing

--- Assets/1_Scripts/Manager/Database.cs
+++ Assets/1_Scripts/Manager/Database.cs
@@ -1,11 +1,8 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using Newtonsoft.Json;
 using NaughtyAttributes;
-using System.IO;
 using System;
+using System.IO;
 using System.Runtime.Serialization.Formatters.Binary;
 
 [System.Serializable]
@@ -21,7 +18,6 @@ public class PlayerData
 public class Database : MonoBehaviour
 {
     public PlayerData playerData;
-    public TextAsset jsonData;
     public string filePath;
     public string fileName;
     private string path;
@@ -33,29 +29,11 @@ public class Database : MonoBehaviour
 
         if (!File.Exists(path))
         {
-            Directory.CreateDirectory(Path.GetDirectoryName(path));
             SaveGame();
-
-            //string json = JsonConvert.SerializeObject(playerData);
-            //File.WriteAllText(path, json);
-
         }
-
-        //string jsonData = File.ReadAllText(path);
-        //playerData = JsonConvert.DeserializeObject<PlayerData>(jsonData);
         LoadGame();
         Assigner();
     }
-
-    //[Button]
-    //public void WriteToJson()
-    //{
-    //#if UNITY_EDITOR
-    //    path = Application.persistentDataPath + filePath + fileName;
-    //#endif
-    //    string json = JsonConvert.SerializeObject(playerData);
-    //    File.WriteAllText(path, json);
-    //}
 
     [Button]
     public void SaveGame()
@@ -69,24 +47,19 @@ public class Database : MonoBehaviour
 
         bf.Serialize(file, playerData);
         file.Close();
-        Debug.Log("Game data saved!");
     }
 
     [Button]
     public void LoadGame()
     {
-        if (File.Exists(Application.persistentDataPath
-                       + filePath + fileName))
+        if (File.Exists(Application.persistentDataPath + filePath + fileName))
         {
             BinaryFormatter bf = new BinaryFormatter();
             FileStream file =
                        File.Open(Application.persistentDataPath + filePath + fileName, FileMode.Open);
             playerData = (PlayerData)bf.Deserialize(file);
             file.Close();
-            Debug.Log("Game data loaded!");
         }
-        else
-            Debug.LogError("There is no save data!");
     }
 
     public void Assigner()
--- Assets/1_Scripts/Manager/ShopManager.cs
+++ Assets/1_Scripts/Manager/ShopManager.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
-using NaughtyAttributes;
 
 [System.Serializable]
 public class Item
